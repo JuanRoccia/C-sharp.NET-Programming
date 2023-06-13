a9a296e0b6e023605dfac053530de1f9840e837b
Juan Manuel Roccia <106710715+JuanRoccia@users.noreply.github.com>
2023-06-13 12:16:16 -0300

    Add files via upload

---
+++ trabajoPractico3Ejercicio6/trabajoPractico3Ejercicio6/CuentaBancaria.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajoPractico3Ejercicio6
+{
+    /* 
+     * En este código, he agregado un nuevo campo cuentaRespaldo 
+     * a la clase CuentaBancaria y lo he inicializado en el 
+     * constructor. Luego, he modificado el método Retiro para 
+     * que retire el dinero de la cuenta de respaldo si no hay 
+     * suficiente saldo en la cuenta principal.
+     */
+    public class CuentaBancaria
+    {
+        private double tasaInteres;
+        private string propietario;
+        private decimal balance;
+        private CuentaBancaria cuentaRespaldo; // Nueva cuenta de respaldo
+
+        public CuentaBancaria(string propietario)
+            : this(propietario, 0.0, 0.0M)
+        {
+        }
+
+        public CuentaBancaria(string propietario, double tasaInteres)
+            : this(propietario, tasaInteres, 0.0M)
+        {
+        }
+
+        public CuentaBancaria(string propietario, double tasaInteres, decimal balance)
+        {
+            this.tasaInteres = tasaInteres;
+            this.propietario = propietario;
+            this.balance = balance;
+            this.cuentaRespaldo = null; // Inicializar la cuenta de respaldo a null
+        }
+
+
+        public decimal Balance()
+        {
+            return balance;
+        }
+
+        public void Retiro(decimal monto)
+        {
+            if (balance >= monto)
+            {
+                balance -= monto;
+            }
+            else if (cuentaRespaldo != null && cuentaRespaldo.Balance() >= monto)
+            {
+                cuentaRespaldo.Retiro(monto);
+            }
+            else
+            {
+                // Crear una nueva cuenta de respaldo si es necesario
+                if (cuentaRespaldo == null)
+                {
+                    cuentaRespaldo = new CuentaBancaria(propietario, tasaInteres, 0);
+                }
+
+                // Verificar si hay suficiente saldo en la cuenta de respaldo
+                if (cuentaRespaldo.Balance() >= monto)
+                {
+                    // Retirar el dinero de la cuenta de respaldo
+                    cuentaRespaldo.Retiro(monto);
+                }
+                else
+                {
+                    // Lanzar una excepción o simplemente no realizar el retiro
+                    throw new Exception("No hay suficiente saldo en la cuenta principal y la cuenta de respaldo para cubrir el monto del retiro.");
+                }
+            }
+        }
+
+        public void Deposito(decimal monto)
+        {
+            balance += monto;
+        }
+
+        public void AddInteres()
+        {
+            balance = balance + balance * (decimal)tasaInteres;
+        }
+
+        public override string ToString()
+        {
+            return "La cuenta bancaria de " + propietario + " tiene un total de $ " + balance;
+        }
+    }
+}
+/* 
+ * En cuanto a la pregunta sobre si es posible modificar el 
+ * estado privado de una cuenta bancaria desde otra, la 
+ * respuesta es no. Los campos privados de una clase solo 
+ * pueden ser accedidos desde dentro de la misma clase. Sin 
+ * embargo, puedes proporcionar métodos públicos en la clase 
+ * que permitan a otras clases interactuar con estos campos 
+ * privados de una manera controlada, como hemos hecho con los 
+ * métodos Retiro, Deposito y Balance.
+ */
---
+++ trabajoPractico3Ejercicio6/trabajoPractico3Ejercicio6/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace trabajoPractico3Ejercicio6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            /// Crear una nueva cuenta bancaria
+            CuentaBancaria cuenta = new CuentaBancaria("Juan", 0.05, 1000M);
+
+            // Imprimir el balance inicial
+            Console.WriteLine(cuenta.ToString());
+
+            // Depositar dinero en la cuenta
+            cuenta.Deposito(500M);
+            Console.WriteLine(cuenta.ToString());
+
+            // Retirar dinero de la cuenta
+            cuenta.Retiro(200M);
+            Console.WriteLine(cuenta.ToString());
+
+            // Retirar una cantidad mayor al balance de la cuenta principal
+            try
+            {
+                cuenta.Retiro(2000M);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ocurrió un error al intentar retirar dinero: " + ex.Message);
+            }
+
+            Console.WriteLine(cuenta.ToString());
+
+            /*En este código, primero creamos una nueva cuenta 
+             * bancaria con un balance inicial de 1000. Luego, 
+             * depositamos 500 en la cuenta y retiramos 200. 
+             * Finalmente, intentamos retirar 2000, que es más 
+             * de lo que tenemos en la cuenta principal. Como 
+             * hemos implementado la lógica para retirar de la 
+             * cuenta de respaldo cuando no hay suficiente saldo 
+             * en la cuenta principal, este retiro debería ser 
+             * exitoso y el balance de la cuenta de respaldo 
+             * debería disminuir.*/
+        }
+    }
+}
